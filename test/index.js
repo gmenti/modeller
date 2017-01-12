8ad13118815778046920e41ac0a1a8ef45da6382
Giuseppe Menti <mentifg@gmail.com>
2017-01-12 15:20:25 -0200

    feat: Add project base

---
+++ test/index.js
@@ -0,0 +1,41 @@
+const Moldelizer = require('../lib/index');
+
+const asyncValidation = () => new Promise((resolve, reject) => {
+  resolve();
+  reject({
+    key: 'email',
+    value: 'unique',
+  });
+});
+
+const peopleValidator = new Moldelizer({
+  name: 'string',
+  age: 'integer|size:6,9',
+}, asyncValidation);
+
+describe('Validator tests', () => {
+
+  it('validator must be a valid', (done) => {
+    peopleValidator.test({
+      name: 'Giuseppe',
+      age: 7,
+    }).then(() => {
+      done();
+    }).catch((errors) => {
+      console.log(errors);
+      done('validator is invalid');
+    });
+  });
+
+	it('validator must be a invalid', (done) => {
+    peopleValidator.test({
+      name: null,
+      age: 10,
+    }).then(() => {
+      done('validator is valid');
+    }).catch(() => {
+      done();
+    });
+	});
+
+});
